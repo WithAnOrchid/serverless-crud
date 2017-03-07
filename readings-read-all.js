30df257eef92ba0ed205b6793e49d95f7a47b7fd
Di Liu <unknowngas@gmail.com>
2017-03-07 02:40:31 -0500

    Try to fix var

--- readings-read-all.js
+++ readings-read-all.js
@@ -6,28 +6,32 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports = (event, callback) => {
   console.log("Event: " + JSON.stringify(event));
 
-sensor_id = "";
-published_at = "";
-
   if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
     if (event.queryStringParameters.sensor_id !== undefined && event.queryStringParameters.sensor_id !== null && event.queryStringParameters.name !== "") {
         console.log("Received sensor_id: " + event.queryStringParameters.sensor_id);
         sensor_id = event.queryStringParameters.sensor_id;
+        if (event.queryStringParameters.published_at !== undefined && event.queryStringParameters.published_at !== null && event.queryStringParameters.published_at !== "") {
+            console.log("Received http published_at: " + event.queryStringParameters.published_at);
+            published_at = event.queryStringParameters.published_at;
+
+              const params = {
+                TableName: 'readings',
+                Key: {
+                  sensor_id: sensor_id ,
+                  published_at: published_at
+                }    
+              };
+            }
         }
 
-    if (event.queryStringParameters.published_at !== undefined && event.queryStringParameters.published_at !== null && event.queryStringParameters.published_at !== "") {
-        console.log("Received http published_at: " + event.queryStringParameters.published_at);
-        published_at = event.queryStringParameters.published_at;
-        }
+  }
+  else
+  {
+      const params = {
+    TableName: 'readings'    
+  };
   }        
 
-  const params = {
-    TableName: 'readings',
-    Key: {
-      sensor_id: sensor_id ,
-      published_at: published_at
-    }    
-  };
 
   console.log("params: " + JSON.stringify(params));
 
