9edb0ba1d680b8d2bad0de1850a0170e70f3ae23
Di Liu <unknowngas@gmail.com>
2017-03-08 00:17:04 -0500

    remove read-since

--- handler.js
+++ handler.js
@@ -2,7 +2,6 @@
 
 const readingsCreate = require('./readings-create.js');
 const readingsReadAll = require('./readings-read-all.js');
-const readingsReadSince = require('./readings-read-since.js');
 const readingsUpdate = require('./readings-update.js');
 const readingsDelete = require('./readings-delete.js');
 
@@ -22,20 +21,6 @@ module.exports.create = (event, context, callback) => {
 
 module.exports.readAll = (event, context, callback) => {
   readingsReadAll(event, (error, result) => {
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*"
-      },
-      body: JSON.stringify(result),
-    };
-
-    context.succeed(response);
-  });
-};
-
-module.exports.readSince = (event, context, callback) => {
-  readingsReadSince(event, (error, result) => {
     const response = {
       statusCode: 200,
       headers: {
