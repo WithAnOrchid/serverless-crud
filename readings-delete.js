32202244fae69d09084cc96ef7aaf4845ef71a51
Di Liu <unknowngas@gmail.com>
2017-03-09 20:13:53 -0500

    Fix update and delete

--- readings-delete.js
+++ readings-delete.js
@@ -4,24 +4,20 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (event, callback) => {
-  const data = JSON.parse(event.body);
   const sensor_id = event.queryStringParameters.sensor_id;
   const published_at = parseInt(event.queryStringParameters.published_at);
-
-  data.sensor_id = sensor_id;
-  data.sensor_id = published_at;
-  var timeInMs = Date.now();
-  data.processed_at = timeInMs;
-
   const params = {
     TableName : 'readings',
-    Item: data
+    Key: {
+      sensor_id: sensor_id,
+      published_at: published_at
+    }
   };
 
-  return dynamoDb.put(params, (error, data) => {
+  return dynamoDb.delete(params, (error, data) => {
     if (error) {
       callback(error);
     }
-    callback(error, params.Item);
+    callback(error, params.Key);
   });
 };
