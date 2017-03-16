5dc629c51899cae57360980f72358b6fa03eb0d4
Di Liu <unknowngas@gmail.com>
2017-03-16 00:13:08 -0400

    Add more parameters

--- readings-read-all.js
+++ readings-read-all.js
@@ -5,127 +5,176 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (event, callback) => {
 
-console.log("Event: " + JSON.stringify(event));
+  console.log("Event: " + JSON.stringify(event));
 
-var sensor_id;
-var params;
-var start_timestamp;
-var end_timestamp;
-var table = "readings";
+  var sensor_id;
+  var start_timestamp;
+  var end_timestamp;
+  var last_sensor_id;
+  var last_published_at;
+  var limit;
+
+  var has_parameters = false;
+  var has_sensor_id = false;
+  var has_start_timestamp = false;
+  var has_end_timestamp = false;
+  var has_last_sensor_id = false;
+  var has_last_published_at = false;
+  var has_limit = false;
+
+  var table = "readings";
+  var params;
 
   if (event.queryStringParameters !== null && 
-      event.queryStringParameters !== undefined) 
+    event.queryStringParameters !== undefined) 
   {
-  	// we got some params from URL
+    has_parameters = true;
     if (event.queryStringParameters.sensor_id !== undefined && 
-        event.queryStringParameters.sensor_id !== null && 
-        event.queryStringParameters.name !== "") 
+      event.queryStringParameters.sensor_id !== null && 
+      event.queryStringParameters.sensor_id !== "") 
     {
-    	// sensor_id is provided
-      console.log("Received sensor_id: " + event.queryStringParameters.sensor_id);
+      has_sensor_id = true;
       sensor_id = event.queryStringParameters.sensor_id;
+    }
+    if (event.queryStringParameters.start_timestamp !== undefined && 
+      event.queryStringParameters.start_timestamp !== null && 
+      event.queryStringParameters.start_timestamp !== "") 
+    {
+      has_start_timestamp = true;
+      start_timestamp = parseInt(event.queryStringParameters.start_timestamp);
+    }
+    if (event.queryStringParameters.end_timestamp !== undefined && 
+      event.queryStringParameters.end_timestamp !== null && 
+      event.queryStringParameters.end_timestamp !== "") 
+    {
+      has_end_timestamp = true;
+      end_timestamp = parseInt(event.queryStringParameters.end_timestamp);
+    }
+    if (event.queryStringParameters.last_sensor_id !== undefined && 
+      event.queryStringParameters.last_sensor_id !== null && 
+      event.queryStringParameters.last_sensor_id !== "") 
+    {
+      has_last_sensor_id = true;
+      last_sensor_id = event.queryStringParameters.last_sensor_id;
+    }
+    if (event.queryStringParameters.last_published_at !== undefined && 
+      event.queryStringParameters.last_published_at !== null && 
+      event.queryStringParameters.last_published_at !== "") 
+    {
+      has_last_published_at = true;
+      last_published_at = event.queryStringParameters.last_published_at;
+    }
+    if (event.queryStringParameters.limit !== undefined && 
+      event.queryStringParameters.limit !== null && 
+      event.queryStringParameters.limit !== "") 
+    {
+      has_limit = true;
+      limit = parseInt(event.queryStringParameters.limit);
+    }
 
-      if (event.queryStringParameters.start_timestamp !== undefined && 
-        event.queryStringParameters.start_timestamp !== null && 
-        event.queryStringParameters.start_timestamp !== "") 
+  }
+  
+  // Start set params
+  if(has_parameters)
+  {
+    // has some params
+    if(has_sensor_id)
+    {
+      // sensor_id is set
+      if(has_start_timestamp)
       {
-      	// start is provided 
-        console.log("Received start_timestamp: " + event.queryStringParameters.start_timestamp);
-        start_timestamp = parseInt(event.queryStringParameters.start_timestamp);
-
-        if (event.queryStringParameters.end_timestamp !== undefined && 
-        	event.queryStringParameters.end_timestamp !== null && 
-        	event.queryStringParameters.end_timestamp !== "") 
-      	{
-      		// all info is provided
-        	console.log("Received end_timestamp: " + event.queryStringParameters.end_timestamp);
-        	end_timestamp = parseInt(event.queryStringParameters.end_timestamp);
-
-	        params = {
-	          TableName: table,
-	          ConsistentRead: true,
-	          KeyConditionExpression:"sensor_id = :sensor_id AND published_at BETWEEN :start_timestamp AND :end_timestamp",
-              ExpressionAttributeValues: {
-            	":sensor_id": sensor_id,
-            	":start_timestamp": start_timestamp,
-            	":end_timestamp": end_timestamp
-            	}    
-	        };
-      	}
-      	else
-      	{
-      		// have sensor_id, start_timestamp, end_timestamp not given
-      		console.log("have sensor_id, start_timestamp, but end_timestamp not given ");
-      		var currTime = Date.now();
-      		params = {
-	          TableName: table,
-	          ConsistentRead: true,
-	          KeyConditionExpression:"sensor_id = :sensor_id AND published_at BETWEEN :start_timestamp AND :end_timestamp",
-              ExpressionAttributeValues: {
-            	":sensor_id": sensor_id,
-            	":start_timestamp": start_timestamp,
-            	":end_timestamp": currTime
-            	}    
-	        };
-      	}
-
+        // sensor_id is set, start_timestamp is set
+        if(has_end_timestamp)
+        {
+          // sensor_id is set, start_timestamp is set, end_timestamp is set
+          params = {
+            TableName: table,
+            ConsistentRead: true,
+            KeyConditionExpression:"sensor_id = :sensor_id AND published_at BETWEEN :start_timestamp AND :end_timestamp",
+            ExpressionAttributeValues: {
+              ":sensor_id": sensor_id,
+              ":start_timestamp": start_timestamp,
+              ":end_timestamp": end_timestamp
+            }    
+          };
+        }
+        else
+        {
+          // sensor_id is set, start_timestamp is set, end_timestamp not set
+          var currTime = Date.now();
+          params = {
+            TableName: table,
+            ConsistentRead: true,
+            KeyConditionExpression:"sensor_id = :sensor_id AND published_at BETWEEN :start_timestamp AND :end_timestamp",
+            ExpressionAttributeValues: {
+              ":sensor_id": sensor_id,
+              ":start_timestamp": start_timestamp,
+              ":end_timestamp": currTime
+            }    
+          };
+        }
       }
       else
       {
-      	// have sensor_id, no start_timestamp
-      	console.log("have sensor_id, no start_timestamp");
-      		params = {
-	          TableName: table,
-	          ConsistentRead: true,
-	          KeyConditionExpression:"sensor_id = :sensor_id",
-              ExpressionAttributeValues: {
-            	":sensor_id": sensor_id
-            }
-	        };
+        // sensor_id is set, start_timestamp not set
+        params = {
+          TableName: table,
+          ConsistentRead: true,
+          KeyConditionExpression:"sensor_id = :sensor_id",
+          ExpressionAttributeValues: {
+            ":sensor_id": sensor_id
+          }
+        };
       }
     }
     else
     {
-    	// no sensor_id
-    	console.log("no sensor_id");
-    	params = {
-      		TableName: 'readings',
-      		ConsistentRead: true   
-    	};
-
-    	return dynamoDb.scan(params, (error, data) => {
-    	if (error) {
-      	callback(error);
-    	}
-    	callback(error, data);
-  		});
+      // no sensor_id
+      params = {
+        TableName: table,
+        ConsistentRead: true   
+      };
     }
-
   }
   else
   {
-  	// we got nothing from URL
-  	console.log("we got nothing from URL");
-
+    // no params
     params = {
       TableName: table,
       ConsistentRead: true    
     };
+  }// end of all if-else block
 
-    return dynamoDb.scan(params, (error, data) => {
+// special condtions
+if(has_last_sensor_id && has_last_published_at)
+{
+  params.ExclusiveStartKey = {
+    sensor_id: last_sensor_id,
+    published_at: last_published_at
+  };
+}
+
+if(has_limit)
+{
+  params.Limit = limit;
+}
+
+// determin call scan or query
+if(!has_parameters || !has_sensor_id)
+{
+  return dynamoDb.scan(params, (error, data) => {
     if (error) {
       callback(error);
     }
     callback(error, data);
   });
-  }//end of URL parameters validation
-
-  console.log('params is ' + JSON.stringify(params));
-
+}
+else
+{
   return dynamoDb.query(params, (error, data) => {
     if (error) {
       callback(error);
     }
     callback(error, data);
   });
-};
+}
