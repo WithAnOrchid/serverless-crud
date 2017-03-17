c88763e675dc4749e06a90556d6bda86b4b2bdf7
Di Liu <unknowngas@gmail.com>
2017-03-17 00:26:26 -0400

    Add entry point for ploting

--- handler.js
+++ handler.js
@@ -4,6 +4,7 @@ const readingsCreate = require('./readings-create.js');
 const readingsReadAll = require('./readings-read-all.js');
 const readingsUpdate = require('./readings-update.js');
 const readingsDelete = require('./readings-delete.js');
+const readingsPlot = require('./readings-plot.js');
 
 module.exports.create = (event, context, callback) => {
   readingsCreate(event, (error, result) => {
@@ -49,6 +50,20 @@ module.exports.update = (event, context, callback) => {
 
 module.exports.delete = (event, context, callback) => {
   readingsDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.plot = (event, context, callback) => {
+  readingsPlot(event, (error, result) => {
     const response = {
       statusCode: 200,
       headers: {
---
+++ readings-plot.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = (event, callback) => {
+
+	console.log("Event: " + JSON.stringify(event));
+
+	var sensor_id;
+	var published_at;
+	var limit;
+
+	var has_parameters = false;
+	var has_sensor_id = false;
+	var has_published_at = false;
+	var has_limit = false;
+
+	var table = "readings";
+	var params;
+
+	if (event.queryStringParameters !== null && 
+		event.queryStringParameters !== undefined) 
+	{
+		has_parameters = true;
+		if (event.queryStringParameters.sensor_id !== undefined && 
+			event.queryStringParameters.sensor_id !== null && 
+			event.queryStringParameters.sensor_id !== "") 
+		{
+			has_sensor_id = true;
+			sensor_id = event.queryStringParameters.sensor_id;
+		}
+		if (event.queryStringParameters.published_at !== undefined && 
+			event.queryStringParameters.published_at !== null && 
+			event.queryStringParameters.published_at !== "") 
+		{
+			has_published_at = true;
+			published_at = parseInt(event.queryStringParameters.last_published_at);
+		}
+		if (event.queryStringParameters.limit !== undefined && 
+			event.queryStringParameters.limit !== null && 
+			event.queryStringParameters.limit !== "") 
+		{
+			has_limit = true;
+			limit = parseInt(event.queryStringParameters.limit);
+		}
+
+	}
+	
+	// Start set params
+	if(has_parameters)
+	{
+		// has some params
+		if(has_sensor_id)
+		{
+			if(has_published_at)
+			{
+				// Take as LastEvaluatedKey
+				params = {
+					TableName: table,
+					ConsistentRead: true,
+					KeyConditionExpression:"sensor_id = :sensor_id",
+					ExpressionAttributeValues: {
+						":sensor_id": sensor_id
+					},
+					ExclusiveStartKey: {
+						"sensor_id": sensor_id,
+						"published_at": published_at
+					}
+				};
+			}
+			else
+			{
+				// Only has sensor_id, to be used as retrive latest x readings
+				// read backwards
+				params = {
+					TableName: table,
+					ConsistentRead: true,
+					KeyConditionExpression:"sensor_id = :sensor_id",
+					ExpressionAttributeValues: {
+						":sensor_id": sensor_id
+					},
+					ScanIndexForward: false
+				};
+			}
+		}
+		else
+		{
+			// no sensor_id
+			params = {
+				TableName: table,
+				ConsistentRead: true,
+				ScanIndexForward: false
+			};
+		}
+	}
+	else
+	{
+		// no params
+		params = {
+			TableName: table,
+			ConsistentRead: true,
+			ScanIndexForward: false
+		};
+	}// end of all if-else block
+
+// limit can be combined into any params
+if(has_limit)
+{
+	params.Limit = limit;
+}
+
+
+console.log("Params: " + JSON.stringify(params));
+
+// determin call scan or query
+if(has_parameters && has_sensor_id && has_published_at)
+{
+	return dynamoDb.query(params, (error, data) => {
+		if (error) {
+			callback(error);
+		}
+		callback(error, data);
+	});
+}
+else
+{
+	return dynamoDb.scan(params, (error, data) => {
+		if (error) {
+			callback(error);
+		}
+		callback(error, data);
+	});
+}
+};
