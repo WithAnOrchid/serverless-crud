8d57ab703af8a41d64efe48e3ececf52dc967496
Di Liu <unknowngas@gmail.com>
2017-05-04 00:06:28 -0400

    Add pmv

--- readings-create.js
+++ readings-create.js
@@ -8,18 +8,127 @@ module.exports = (event, callback) => {
   const data = JSON.parse(event.body);
 
   //data.id = uuid.v1();
-  var timeInMs = Date.now();
-  data.processed_at = timeInMs;
+  //var timeInMs = Date.now();
+  data.published_at = Date.now();
+  data.processed_at = Date.now();
 
-  const params = {
-    TableName: 'readings',
-    Item: data
-  };
+  // Special case for TH module
+  if(data.temperature_reading != undefined &&
+   data.temperature_reading != null)
+  {
 
-  return dynamoDb.put(params, (error, data) => {
-    if (error) {
-      callback(error);
-    }
-    callback(error, params.Item);
+    // NEW CHANGE
+    var lambda = new AWS.Lambda({
+    region: 'us-east-1' //change to your region
   });
+    const event = {
+      "queryStringParameters": {
+        "rh": data.temperature_reading,
+        "ta": data.humidity_reading
+      }
+    };
+
+    lambda.invoke({
+      FunctionName: 'pmv-dev-computePMV',
+    Payload: JSON.stringify(event, null, 2) // pass params
+  }, function(error, data) {
+    if (error) {
+        //context.done('error', error);
+        console.log(error);
+      }
+      if(data.Payload){
+        //context.succeed(data.Payload)
+        const temp = JSON.parse(data.Payload);
+        const result = JSON.parse(temp.body);
+        const PMV = result.PMV;
+        const APMV = result.apmv;
+        const PPD = result.ppd;
+      }
+    });
+
+    
+
+    // Split the single JSON to 2 JSONs
+    var data1 = {
+      sensor_id: 'TEMPERATURE-' + data.sensor_id,
+      device_id: data.device_id,
+      device_type: data.device_type,
+      sensor_type: 'temperature',
+      sensor_reading: data.temperature_reading,
+      published_at: data.published_at,
+      processed_at: data.processed_at
+    };
+    var data2 = {
+      sensor_id: 'HUMIDITY-' + data.sensor_id,
+      device_id: data.device_id,
+      device_type: data.device_type,
+      sensor_type: 'humidity',
+      sensor_reading: data.humidity_reading,
+      published_at: data.published_at,
+      processed_at: data.processed_at
+    };
+
+    var data3 = {
+      sensor_id: 'PMV-' + data.sensor_id,
+      device_id: data.device_id,
+      device_type: data.device_type,
+      sensor_type: 'PMV_NOT_A_SENSOR',
+      pmv: PMV,
+      apmv: APMV,
+      ppd: PPD,
+      published_at: data.published_at,
+      processed_at: data.processed_at
+    };
+    
+    var params1 = {
+      TableName: 'readings',
+      Item: data1
+    };
+    var params2 = {
+      TableName: 'readings',
+      Item: data2
+    };
+
+    var params3 = {
+      TableName: 'readings',
+      Item: data3
+    };
+    
+    dynamoDb.put(params1, function(err, data) {
+      if (err) console.log("Unable to update item. Error: ", JSON.stringify(err, null, 2));
+      else console.log("Updated item succeeded: ", JSON.stringify(data, null, 2));
+            //next() // modify for err handling
+          });
+
+    dynamoDb.put(params2, function(err, data) {
+      if (err) console.log("Unable to update item. Error: ", JSON.stringify(err, null, 2));
+      else console.log("Updated item succeeded: ", JSON.stringify(data, null, 2));
+            //next() // modify for err handling
+          });
+
+    return dynamoDb.put(params3, (error, data) => {
+      if (error) {
+        callback(error);
+      }
+      callback(error, params3.Item);
+    });
+    
+    
+
+  } // end of if
+  else
+  {
+    const params = {
+      TableName: 'readings',
+      Item: data
+    };
+
+    return dynamoDb.put(params, (error, data) => {
+      if (error) {
+        callback(error);
+      }
+      callback(error, params.Item);
+    });
+
+}// end of else
 };
